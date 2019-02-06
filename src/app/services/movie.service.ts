5d056a180f7ff670885891787a663e3026597f01
Philip Wisner <philipwisner@gmail.com>
2019-02-06 13:09:10 -0500

    added api calls

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -16,6 +16,33 @@ export enum SearchType {
   providedIn: 'root'
 })
 export class MovieService {
+  url = 'http://omdbapi.com/';
+  apiKey = '624481e0';
 
-  constructor() { }
+  //using HttpClient as http variable
+  constructor(private http: HttpClient) { }
+
+/**
+ * Get data from the OmdbApi and map the result to return only the results that we need
+ *
+ * @param {string} title Search Term
+ * @param {SearchType} type movie, series, episode or empty
+ * @returns Observable with the search results
+ */
+  searchData(title: string, type: SearchType): Observable<any> {
+    return this.http.get(`${this.url}?s=${encodeURI(title)}&type=${type}&apikey=${this.apiKey}`).pipe(
+      map(results => results['Search'])
+    );
+  }
+
+  /**
+  * Get the detailed information for an ID using the "i" parameter
+  *
+  * @param {string} id to retrieve information
+  * @returns Observable with detailed information
+  */
+  getDetails(id: any) {
+    return this.http.get(`${this.url}?i=${id}&plot=full&apikey=${this.apiKey}`);
+  }
+
 }
