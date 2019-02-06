3443a3fb8e7eb7e004919fdd79a71e178866d4d2
Philip Wisner <philipwisner@gmail.com>
2019-02-06 13:13:56 -0500

    adding function to page

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -1,4 +1,6 @@
+import { MovieService, SearchType } from './../../services/movie.service';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movies',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesPage implements OnInit {
 
-  constructor() { }
+  results: Observable<any>;
+  searchTerm: string = '';
+  type: SearchType = SearchType.all;
+
+  //using movie service to fetch data from API
+  constructor(private movieSearch: MovieService) { }
 
   ngOnInit() {
+  }
+
+  //function runs when interacting with input, makes api call with passing term and type
+  searchChanged() {
+    this.results = this.movieSearch.searchData(this.searchTerm, this.type);
   }
 
 }
