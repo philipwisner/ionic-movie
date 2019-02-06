f74c19554983a86abe4c96f6493e494b47fd1731
Philip Wisner <philipwisner@gmail.com>
2019-02-06 13:03:12 -0500

    adding service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+// Typescript custom enum for search types (optional)
+export enum SearchType {
+  all = '',
+  movie = 'movie',
+  series = 'series',
+  episode = 'episode',
+}
+
 
 @Injectable({
   providedIn: 'root'
