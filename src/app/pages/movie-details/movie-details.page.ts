5ea38409de94d817e3f7e5b0b1d998ec2760ec42
Philip Wisner <philipwisner@gmail.com>
2019-02-06 13:43:54 -0500

    adding open details page

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,6 @@
+import { MovieService } from "./../../services/movie.service";
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-movie-details',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieDetailsPage implements OnInit {
 
-  constructor() { }
+  information: Object = null;
+
+  /**
+  * @param activatedRoute Information about the route we are on
+  * @param movieService The movie Service to get data
+  */
+  constructor(private activatedRoute: ActivatedRoute, private movieSearch: MovieService) { }
 
   ngOnInit() {
+    // Get the ID that was passed with the URL
+    let id = this.activatedRoute.snapshot.paramMap.get('id');
+
+    // Get the information from the API
+    this.movieSearch.getDetails(id).subscribe(result => {
+      this.information = result;
+    });
+  }
+
+  openWebsite() {
+    window.open(this.information.Website, '_blank');
   }
 
 }
