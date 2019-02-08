7ac5e46001e6a863bacccdb6cf7220403df4861d
Philip Wisner <philipwisner@gmail.com>
2019-02-08 16:34:34 -0500

    added profile page and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,31 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'movies', pathMatch: 'full' },
-  { path: 'movies', loadChildren: './pages/movies/movies.module#MoviesPageModule' },
-  { path: 'movies/:id', loadChildren: './pages/movie-details/movie-details.module#MovieDetailsPageModule' },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
+    path: 'movies',
+    loadChildren: './pages/movies/movies.module#MoviesPageModule'
+  },
+  {
+    path: 'movies/:id',
+    loadChildren: './pages/movie-details/movie-details.module#MovieDetailsPageModule'
+  },
+  {
+    path: 'favorites',
+    loadChildren: './favorites/favorites.module#FavoritesPageModule'
+  },
+  {
+    path: 'profile',
+    loadChildren: './profile/profile.module#ProfilePageModule'
+  },
 ];
 
 @NgModule({
