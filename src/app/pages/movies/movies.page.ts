e3d3951a8bbf7f8586d1a20476d6c649b8c9b0df
Philip Wisner <philipwisner@gmail.com>
2019-02-07 20:50:44 -0500

    set results to subscribe to observable

--- src/app/pages/movies/movies.page.ts
+++ src/app/pages/movies/movies.page.ts
@@ -12,6 +12,7 @@ export class MoviesPage implements OnInit {
   results: Observable<any>;
   searchTerm: string = '';
   type: SearchType = SearchType.all;
+  loading: boolean = false;
 
   //using movie service to fetch data from API
   constructor(private movieSearch: MovieService) { }
@@ -20,8 +21,14 @@ export class MoviesPage implements OnInit {
   }
 
   //function runs when interacting with input, makes api call with passing term and type
+  //show loading while request is being processed
   searchChanged() {
-    this.results = this.movieSearch.searchData(this.searchTerm, this.type);
+    this.loading = true;
+    // this.results = this.movieSearch.searchData(this.searchTerm, this.type);
+    this.movieSearch.searchData(this.searchTerm, this.type).subscribe(res => {
+      this.results = res;
+      this.loading = false;
+    });
   }
 
 }
